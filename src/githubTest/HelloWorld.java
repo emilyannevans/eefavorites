2993db3a2b82442702adf36317cafa1d6a7d3196
Daniel Lewinsohn <d_lewinsohn@coloradocollege.edu>
2020-09-07 14:50:24 -0600

    added bye line to helloworld

--- src/githubTest/HelloWorld.java
+++ src/githubTest/HelloWorld.java
@@ -1,11 +1,13 @@
 package githubTest;
 
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
 		System.out.println("Hello world!");
+		System.out.println("Bye -daniel");
 	}
 
 }
